83ca0e8c66117c5e6f05093fd6b1e057883936f3
jose murilo sodre da mota filho <zehmurilo@172-16-3-22.0000.ufpe.br.iosda.org>
2017-10-01 15:37:44 -0300

    corrigido nome/ordem dos pinos

--- ProjetoFinal/DescricaoTrajetoViewController.swift
+++ ProjetoFinal/DescricaoTrajetoViewController.swift
@@ -35,24 +35,25 @@ class DescricaoTrajetoViewController: UIViewController, UICollectionViewDataSour
     }
     func initPinos () {
         pinos.append(Pinos(latitude: -8.05649, longitude: -34.9535, nome: "Area2"))
-        pinos.append(Pinos(latitude:-8.05768,longitude:-34.95219,nome:"Parada externa CCEN"))
-        pinos.append(Pinos(latitude:-8.05573,longitude:-34.95115,nome:"Saída Principal Cin"))
+        pinos.append(Pinos(latitude:-8.05768,longitude:-34.95219,nome:"CCEN - Parada Externa"))
+        pinos.append(Pinos(latitude:-8.05573,longitude:-34.95115,nome:"CIn - Saída Principal"))
         pinos.append(Pinos(latitude:-8.05512,longitude: -34.95275,nome:"Niate CTG"))
-        pinos.append(Pinos(latitude:-8.05404,longitude: -34.9536,nome:"Parada Interna CTG/Oceanografia"))
-        pinos.append(Pinos(latitude:-8.05281,longitude: -34.95397,nome:"Entrada Principal CTG"))
-        pinos.append(Pinos(latitude:-8.05288,longitude:-34.95557,nome:"Parada Externa CTG"))
-        pinos.append(Pinos(latitude:-8.05157,longitude: -34.95331,nome:"Parada Interna CAC"))
-        pinos.append(Pinos(latitude:-8.0505,longitude:-34.95387,nome:"Entradas Principais CAC/CFCH"))
-        pinos.append(Pinos(latitude:-8.04963,longitude:-34.95464,nome:"Parada Externa CAC/CFCH"))
+        pinos.append(Pinos(latitude:-8.05404,longitude: -34.9536,nome:"CTG - Parada Interna"))
+        pinos.append(Pinos(latitude:-8.05281,longitude: -34.95397,nome:"CTG - Entrada Principal"))
+        pinos.append(Pinos(latitude:-8.05288,longitude:-34.95557,nome:"CTG - Parada Externa"))
+        pinos.append(Pinos(latitude:-8.05157,longitude: -34.95331,nome:"CAC - Parada Interna"))
+        pinos.append(Pinos(latitude:-8.0505,longitude:-34.95387,nome:"CAC/CFCH - Entradas Principais"))
+        pinos.append(Pinos(latitude:-8.04963,longitude:-34.95464,nome:"CAC/CFCH - Parada Externa"))
         pinos.append(Pinos(latitude:-8.04947,longitude:-34.95386,nome:"CE"))
         pinos.append(Pinos(latitude:-8.04927,longitude: -34.95238,nome:"Niate CFCH/CCSA"))
         pinos.append(Pinos(latitude:-8.04858,longitude: -34.95117,nome:"CCSA"))
-        pinos.append(Pinos(latitude:-8.04644,longitude: -34.95029,nome:"Saída Externa CCSA/Corpo Discente"))
-        pinos.append(Pinos(latitude:-8.04758,longitude:-34.94536,nome:"Parada HC-BR101"))
-        pinos.append(Pinos(latitude:-8.05056,longitude: -34.94599,nome:"Parada Externa CCS"))
-        pinos.append(Pinos(latitude:-8.05407,longitude: -34.94659,nome:"Parada Externa Grêmio Estudantil"))
-        pinos.append(Pinos(latitude:-8.05183,longitude:-34.94809,nome:"Parada CCB/Niate CCB"))
+        pinos.append(Pinos(latitude:-8.04644,longitude: -34.95029,nome:"CCSA/Corpo Discente - Saída Externa"))
+        pinos.append(Pinos(latitude:-8.04758,longitude:-34.94536,nome:"HC-BR101 - Parada"))
+        pinos.append(Pinos(latitude:-8.05056,longitude: -34.94599,nome:"CCS - Parada Externa"))
+        pinos.append(Pinos(latitude:-8.05407,longitude: -34.94659,nome:"Grêmio Estudantil - Parada Externa"))
+        pinos.append(Pinos(latitude:-8.05183,longitude:-34.94809,nome:"CCB/Niate CCB - Parada"))
         pinos.append(Pinos(latitude:-8.05402,longitude:-34.94829,nome:"Centro de Educação Física"))
+        pinos.append(Pinos(latitude:-8.051296,longitude:-34.951412,nome:"Biblioteca Central - Parada"))
         
         for pino in pinos {
             pino.adcPonto(mapView)
@@ -74,6 +75,14 @@ class DescricaoTrajetoViewController: UIViewController, UICollectionViewDataSour
         cell.fotoPessoa.image = UIImage (named: pessoas[indexPath.row][0])
         cell.nome.text = pessoas[indexPath.row][1]
         return cell
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor(colorLiteralRed: 0.78, green:  0.192, blue:  0.141, alpha: 1)
+        renderer.lineWidth = 4.0
+        
+        return renderer
     }
     
     
